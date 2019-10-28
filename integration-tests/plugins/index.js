e42101d8d3d90403e165bd824598a84880d785de
Paul Massey <paul.massey@scriptwerx.io>
2019-10-28 15:19:02 +0000

    Cleanup
    Signed-off-by: Paul Massey <paul.massey@scriptwerx.io>

--- integration-tests/plugins/index.js
+++ integration-tests/plugins/index.js
@@ -9,19 +9,15 @@
 // ***********************************************************
 
 const { cypressConfigResolver } = require('../config/cypress-config-resolver');
-const pluginExecuteCommand = require('./plugin-execute-command');
 const cucumber = require('cypress-cucumber-preprocessor').default;
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-module.exports = (on, config) => {
+module.exports = (on) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
   on('file:preprocessor', cucumber());
-  on('task', {
-    pluginExecuteCommand,
-  });
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
   return cypressConfigResolver();
