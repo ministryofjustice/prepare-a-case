eb9782a9fe4a2d667a66bc542de26556fb3722fe
Paul Massey <paul.massey@scriptwerx.io>
2019-10-29 15:58:24 +0000

    Included healthcheck middleware and unit tests
    Signed-off-by: Paul Massey <paul.massey@scriptwerx.io>

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', function (req, res) {
-  res.status(200).send('up')
+  res.status(200).send('pong')
 })
 
 module.exports = router
--- tests/routes/view.test.js
+++ tests/routes/view.test.js
@@ -1,16 +1,31 @@
 const request = require('supertest')
-const app = require('../../app')
+const healthcheck = require('../../routes/middleware/healthcheck')
+
+let app
 
 describe('Routes', () => {
+
+  jest.spyOn(healthcheck, 'health').mockImplementation(function (req, res, next) {
+    return next()
+  })
+
+  beforeEach(() => {
+    app = require('../../app')
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
   it('default route should return status 200', () => {
-    return request(app).get('/').then(res => {
-      expect(res.statusCode).toEqual(200)
+    return request(app).get('/').then(response => {
+      expect(response.statusCode).toEqual(200)
     })
   })
 
   it('case list route should return status 200', () => {
-    return request(app).get('/case-list').then(res => {
-      expect(res.statusCode).toEqual(200)
+    return request(app).get('/case-list').then(response => {
+      expect(response.statusCode).toEqual(200)
     })
   })
 })
