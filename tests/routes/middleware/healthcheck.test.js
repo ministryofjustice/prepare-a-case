8e71dc9940d4f44ec24fe0ea99f1be50c4347495
Paul Massey <paul.massey@scriptwerx.io>
2019-10-31 14:54:55 +0000

    Includes test for env var in health check
    Updated README
    
    Signed-off-by: Paul Massey <paul.massey@scriptwerx.io>

--- tests/routes/middleware/healthcheck.test.js
+++ tests/routes/middleware/healthcheck.test.js
@@ -5,10 +5,15 @@ const { health } = require('../../../routes/middleware/healthcheck')
 const mockNext = jest.fn()
 const resolveObj = { status: 200 }
 const reqObj = { healthy: false }
+const oldEnv = process.env
+let axiosSpy
 
 describe('Health check middleware', () => {
   beforeEach(() => {
-    jest.spyOn(axios, 'get').mockImplementation(url => {
+    jest.resetModules()
+    process.env = { ...oldEnv }
+
+    axiosSpy = jest.spyOn(axios, 'get').mockImplementation(url => {
       return new Promise(resolve => {
         process.nextTick(() =>
           resolve(resolveObj)
@@ -64,6 +69,19 @@ describe('Health check middleware', () => {
 
     it('should continue', () => {
       expect(mockNext).toBeCalled()
+    })
+  })
+
+  describe('Health check should use environment variables for court-case-service URL', () => {
+    it('should GET from localhost if no env var is set', async () => {
+      await health(reqObj, {}, mockNext)
+      expect(axiosSpy).toBeCalledWith('http://localhost:8082/ping')
+    })
+
+    it('should GET from the URL supplied as env var', async () => {
+      process.env.COURT_CASE_SERVICE_URL = 'http://court_case_service.url'
+      await health(reqObj, {}, mockNext)
+      expect(axiosSpy).toBeCalledWith('http://court_case_service.url/ping')
     })
   })
 })
