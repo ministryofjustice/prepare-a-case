b4be264c8e8ecd41b2002afb363a5d10ed8536e8
Paul Massey <paul.massey@scriptwerx.io>
2019-11-20 14:04:23 +0000

    Updated to allow GET on health to support CORS
    Updated dependencies
    
    Signed-off-by: Paul Massey <paul.massey@scriptwerx.io>

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,9 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', function (req, res) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   res.status(200).send('pong')
 })
 
