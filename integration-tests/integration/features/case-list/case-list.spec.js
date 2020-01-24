649d907aa5ed29fb3456b35a3cb807ed8feae5cf
Paul Massey <paul.massey@scriptwerx.io>
2020-01-24 10:02:01 +0000

    Updated test data
    Updated BDD tests to correctly test list table
    
    Signed-off-by: Paul Massey <paul.massey@scriptwerx.io>

--- integration-tests/integration/features/case-list/case-list.spec.js
+++ integration-tests/integration/features/case-list/case-list.spec.js
@@ -2,24 +2,32 @@
 import { And } from 'cypress-cucumber-preprocessor/steps'
 import moment from 'moment'
 
-And('I should see sub navigation with three dates', () => {
+And('I should see sub navigation with default dates', () => {
   cy.get('.moj-sub-navigation__link').eq(0).contains(moment().format('dddd, D MMMM YYYY'))
   cy.get('.moj-sub-navigation__link').eq(1).contains(moment().add(1, 'days').format('dddd, D MMMM YYYY'))
   cy.get('.moj-sub-navigation__link').eq(2).contains(moment().add(2, 'days').format('dddd, D MMMM YYYY'))
 })
 
-And('I should see a table with specific headings', () => {
-  cy.get('.govuk-table__header').eq(0).contains('Defendant')
-  cy.get('.govuk-table__header').eq(1).contains('Probation record')
-  cy.get('.govuk-table__header').eq(2).contains('Offence')
-  cy.get('.govuk-table__header').eq(3).contains('Listing')
-  cy.get('.govuk-table__header').eq(4).contains('Session')
-  cy.get('.govuk-table__header').eq(5).contains('Court')
+And('I should see the following table', $data => {
+  $data.raw()[0].forEach((text, index) => {
+    cy.get('.govuk-table__head > .govuk-table__row').within(() => {
+      cy.get('.govuk-table__header').eq(index).contains(text)
+    })
+  })
+
+  $data.rows().forEach((row, index) => {
+    row.forEach((text, index2) => {
+      cy.get('.govuk-table__body > .govuk-table__row').eq(index).within(() => {
+        cy.get('.govuk-table__cell').eq(index2).contains(text)
+      })
+    })
+  })
 })
 
-And('I should see a list of defendants', () => {
-  cy.get('.govuk-link.govuk-link--no-visited-state').eq(0).contains('JCONE')
-  cy.get('.govuk-link.govuk-link--no-visited-state').eq(1).contains('Mr Joe JMBBLOGGS')
+And('The following defendant names should be links', $data => {
+  $data.raw()[0].forEach((string, index) => {
+    cy.get('.govuk-link.govuk-link--no-visited-state').eq(index).contains(string)
+  })
 })
 
 And('I should not see the table list', () => {
