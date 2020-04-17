3b098bea14a99a6d697d319a81dce6878aeb4472
Paul Massey <paul.massey@digital.justice.gov.uk>
2020-04-17 15:13:30 +0100

    Updated unit tests
    Signed-off-by: Paul Massey <paul.massey@digital.justice.gov.uk>

--- services/community-service.js
+++ services/community-service.js
@@ -10,7 +10,6 @@ const getRequirements = async (convictions, crn) => {
   return Promise.all(
     convictions.map(async conviction => {
       if (conviction.active && conviction.sentence) {
-        console.log(`Now calling for requirements: ${apiUrl}/offender/${crn}/convictions/${conviction.convictionId}/requirements`)
         const res = await request(`${apiUrl}/offender/${crn}/convictions/${conviction.convictionId}/requirements`)
         return { ...conviction, ...res.data }
       } else {
@@ -21,7 +20,6 @@ const getRequirements = async (convictions, crn) => {
 }
 
 const getProbationRecord = async crn => {
-  console.log(`Now calling for probation-record: ${apiUrl}/offender/${crn}/probation-record`)
   const res = await request(`${apiUrl}/offender/${crn}/probation-record`)
   if (res.data && res.data.convictions) {
     const enrichedConvictions = await getRequirements(res.data.convictions, crn)
