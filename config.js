08469138db7dd161409acd4336fc2d240382c60c
Paul Massey <paul.massey@digital.justice.gov.uk>
2020-07-10 09:11:19 +0100

    Removed required in production flag from court information
    Signed-off-by: Paul Massey <paul.massey@digital.justice.gov.uk>

--- config.js
+++ config.js
@@ -17,8 +17,8 @@ const requiredInProduction = { requireInProduction: true }
 
 module.exports = {
   settings: {
-    courtCode: get('COURT_CODE', 'SHF', requiredInProduction),
-    courtName: get('COURT_NAME', 'Sheffield Magistrates\' Court', requiredInProduction),
+    courtCode: get('COURT_CODE', 'SHF'),
+    courtName: get('COURT_NAME', 'Sheffield Magistrates\' Court'),
     casesPerPage: get('CASES_PER_PAGE', 20),
     casesTotalDays: get('CASES_TOTAL_DAYS', 3)
   },
