a87bc6a619ed096a5ace546b106217c034a73a25
katie-cawthorne <katie.cawthorne@digital.justice.gov.uk>
2020-07-16 15:48:53 +0100

    :loud_sound: Add application insights

--- server/index.js
+++ server/index.js
@@ -1,7 +1,14 @@
+// Do appinsights first as it does some magic instrumentation work, i.e. it affects other 'require's
+// In particular, applicationinsights automatically collects bunyan logs
+const appInsightsClient = require('./utils/azure-appinsights')
+
 const createApp = require('../app')
 const authClientBuilder = require('./data/authClientBuilder')
 const createSignInService = require('./authentication/signInService')
 const createUserService = require('./services/userService')
+
+// eslint-disable-next-line no-unused-vars
+const eventPublisher = require('./services/eventPublisher')(appInsightsClient)
 
 const userService = createUserService(authClientBuilder)
 
