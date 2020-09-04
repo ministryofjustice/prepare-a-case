9825f82aefbd698b7fea3ea2efa3eeafbdb0bd40
Paul Massey <paul.massey@digital.justice.gov.uk>
2020-09-04 10:00:33 +0100

    :sparkles: Included match link confirmation messages
    Included a11y testing for the matching journeys
    
    Signed-off-by: Paul Massey <paul.massey@digital.justice.gov.uk>

--- integration-tests/integration/features/matching/matching.spec.js
+++ integration-tests/integration/features/matching/matching.spec.js
@@ -58,6 +58,10 @@ And('I should see the defendant record options', $data => {
   })
 })
 
+And('I should see the match linked banner message', () => {
+  cy.get('.moj-banner__message').contains(`You have successfully linked an nDelius record to ${world.data.defendantName}.`)
+})
+
 And('I should see the match confirmation banner message', () => {
   cy.get('.moj-banner__message').contains(`You have successfully confirmed a record for ${world.data.defendantName}.`)
 })
