404472e8b3b20d45c89d28e1642659f09164ad5e
Paul Massey <paul.massey@digital.justice.gov.uk>
2020-10-06 10:57:57 +0100

    :bug: Fix incorrect headers for attachments
    Signed-off-by: Paul Massey <paul.massey@digital.justice.gov.uk>

--- server/routes/attachments.js
+++ server/routes/attachments.js
@@ -8,9 +8,9 @@ module.exports = function Index ({ authenticationMiddleware }) {
 
   router.get('/:crn/documents/:documentId/:documentName', async (req, res) => {
     const file = await getAttachment(req.params.crn, req.params.documentId)
-    res.setHeader('Content-disposition', `attachment; filename=${req.params.documentName}`)
-    res.setHeader('Content-type', 'application/octet-stream')
-    res.send(file)
+    res.setHeader('Content-disposition', file.headers['Content-disposition'])
+    res.setHeader('Content-type', file.headers['Content-type'])
+    res.send(file.data)
   })
   return router
 }
