282bb3ac173d0f64ad54d35267c590b8b8c3c030
Katie Cawthorne <51707463+katie-cawthorne@users.noreply.github.com>
2020-10-14 09:22:21 +0100

    Pic 288 risk register (#201)
    * :sparkles: PIC-288: Added risk register feature
    
    * :art: PIC-288: PR changes
    
    * :green_heart: PIC-288: Fixed integration tests
    
    * :bug: PIC-288: Fix display of risk data
    
    * :green_heart: PIC-288: Fixed lint issue

--- server/routes/index.js
+++ server/routes/index.js
@@ -166,11 +166,8 @@ module.exports = function Index ({ authenticationMiddleware }) {
     templateValues.title = 'Risk register'
 
     const crn = templateValues.data.crn
-    const riskResponse = await getRiskDetails(crn)
 
-    templateValues.data.riskData = {
-      ...riskResponse
-    }
+    templateValues.data.riskData = await getRiskDetails(crn)
 
     templateValues.params = {
       ...templateValues.params
