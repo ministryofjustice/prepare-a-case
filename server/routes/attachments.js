98eed5653784b21c55ddabb9ddfce3f10bc46946
Paul Massey <paul.massey@digital.justice.gov.uk>
2020-11-06 12:01:59 +0000

    :bug: File request failure handled accordingly
    Signed-off-by: Paul Massey <paul.massey@digital.justice.gov.uk>

--- server/routes/attachments.js
+++ server/routes/attachments.js
@@ -6,10 +6,14 @@ module.exports = function Index ({ authenticationMiddleware }) {
 
   router.use(authenticationMiddleware())
 
-  router.get('/:crn/documents/:documentId/:documentName', async (req, res) => {
+  router.get('/:crn/documents/:documentId/:documentName', async (req, res, next) => {
     const file = await getAttachment(req.params.crn, req.params.documentId)
-    res.attachment(req.params.documentName)
-    file.data.pipe(res)
+    if (file && file.data) {
+      res.attachment(req.params.documentName)
+      file.data.pipe(res)
+    } else {
+      next()
+    }
   })
   return router
 }
