8afbb6b93865f2d253eaaedd2afd77b43388287d
Paul Massey <paul.massey@digital.justice.gov.uk>
2021-01-05 17:26:05 +0000

    :recycle: Refactored contract tests to use pact mock server
    Signed-off-by: Paul Massey <paul.massey@digital.justice.gov.uk>

--- tests/jest.pact.config.js
+++ tests/jest.pact.config.js
@@ -1,13 +1,7 @@
 module.exports = {
   testEnvironment: 'node',
   verbose: true,
-  clearMocks: true,
-  collectCoverage: false,
   rootDir: './',
-  reporters: [
-    'default',
-    'jest-junit'
-  ],
   testMatch: [
     '/**/?(*.)(spec|test).pact.{js,jsx,mjs}'
   ]
