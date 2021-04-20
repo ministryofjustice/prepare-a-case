5c1bb438849857758f6581d03f84918fafd13534
katie-cawthorne <katie.cawthorne@digital.justice.gov.uk>
2021-04-20 16:51:28 +0100

    :sparkles: PIC-1287: add privacy notice

--- server/routes/index.js
+++ server/routes/index.js
@@ -49,6 +49,10 @@ module.exports = function Index ({ authenticationMiddleware }) {
     res.render('accessibility-statement', { params: { nonce: nonce } })
   })
 
+  router.get('/privacy-notice', (req, res) => {
+    res.render('privacy-notice', { params: { nonce: nonce } })
+  })
+
   router.get('/my-courts', async (req, res) => {
     const { session } = req
     const userSelectedCourts = await getUserSelectedCourts(res.locals.user.userId)
