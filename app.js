ffa2b5b50e4f2a54a7f1322915186c4effce8a5a
theDustRoom <paul.massey@digital.justice.gov.uk>
2021-04-27 16:46:41 +0100

    :bug: Updated CSP to allow all GA connections/scripts
    Signed-off-by: theDustRoom <paul.massey@digital.justice.gov.uk>

--- app.js
+++ app.js
@@ -39,18 +39,34 @@ module.exports = function createApp ({ signInService, userService }) {
   app.use(helmet({
     contentSecurityPolicy: {
       directives: {
-        defaultSrc: ['\'self\'', 'www.google-analytics.com'],
+        defaultSrc: [
+          '\'self\'',
+          'https://www.google-analytics.com',
+          'www.google-analytics.com'
+        ],
         objectSrc: ['\'none\''],
         scriptSrc: [
           '\'self\'',
+          'www.google-analytics.com',
+          'https://www.google-analytics.com',
+          'https://www.googletagmanager.com',
           '\'sha256-6cE0E4X9g7PbRlMR/+GoyLM4W7mjVxZL4H6E8FgY8OA=\'',
           '\'sha256-l1eTVSK8DTnK8+yloud7wZUqFrI0atVo6VlC6PJvYaQ=\'',
           '\'sha256-Ex+PXm59nVbu/S+FH/u8FLio5zO5YfFPo0/jH0uw19k=\'',
           '\'sha256-QIG/FBh5vORMkpviiAyUOvMgp6XvwQIEagSXO2FUmyo=\'',
           `'nonce-${config.nonce}'`
         ],
+        imgSrc: [
+          '\'self\'',
+          'https://www.google-analytics.com',
+          'www.google-analytics.com'
+        ],
         upgradeInsecureRequests: [],
-        connectSrc: ['\'self\'', 'www.google-analytics.com']
+        connectSrc: [
+          '\'self\'',
+          'www.google-analytics.com',
+          'https://www.google-analytics.com'
+        ]
       }
     }
   }))
