5630e62c88a557853a4ec0b1cb7e653eb6001c83
theDustRoom <paul.massey@digital.justice.gov.uk>
2021-11-30 15:54:01 +0000

    🔧 Adjusted timeouts
    Signed-off-by: theDustRoom <paul.massey@digital.justice.gov.uk>

--- config.js
+++ config.js
@@ -19,8 +19,8 @@ const requiredInProduction = { requireInProduction: true }
 
 module.exports = {
   settings: {
-    defaultTimeout: 2000,
-    healthTimeout: 1000,
+    defaultTimeout: 4000,
+    healthTimeout: 2000,
     casesPerPage: get('CASES_PER_PAGE', 20),
     casesTotalDays: get('CASES_TOTAL_DAYS', 7),
     casesExcludedDays: get('CASES_EXCLUDED_DAYS', '0'), // Coma delimited String of days to exclude, incremental from 0 (Sunday)
