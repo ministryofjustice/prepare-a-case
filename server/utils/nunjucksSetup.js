cc67a215ce934c163880c3e701993358ad0586f8
theDustRoom <paul.massey@digital.justice.gov.uk>
2022-03-03 10:03:02 +0000

    Added unique Nunjucks Array filter
    Corrected display of CP listing numbers
    
    Signed-off-by: theDustRoom <paul.massey@digital.justice.gov.uk>

--- server/utils/nunjucksSetup.js
+++ server/utils/nunjucksSetup.js
@@ -67,5 +67,7 @@ module.exports = (app, path) => {
     return sourceString.includes('Courtroom') ? sourceString.replace(/([A-Za-z 0]*)?/, '') : sourceString.replace(/([0]*)?/, '')
   })
 
+  env.addFilter('unique', arr => arr instanceof Array && arr.filter((e, i, arr) => arr.indexOf(e) === i) || arr)
+
   env.addGlobal('googleAnalyticsKey', googleAnalyticsKey)
 }
