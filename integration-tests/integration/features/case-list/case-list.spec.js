3b45ad98db8c2defac6c34c7c1d4e4dd73929740
theDustRoom <paul.massey@digital.justice.gov.uk>
2022-03-04 10:41:43 +0000

    ✅ Updated integration tests to check specific case list table cell contents
    Signed-off-by: theDustRoom <paul.massey@digital.justice.gov.uk>

--- integration-tests/integration/features/case-list/case-list.spec.js
+++ integration-tests/integration/features/case-list/case-list.spec.js
@@ -75,6 +75,30 @@ And('I click pagination link {string}', $string => {
   cy.get('.moj-pagination__link').contains($string).click()
 })
 
+And('Defendant {string} should display the following {string} data', ($defendant, $header, $data) => {
+  let headerIndex
+  cy.contains('th', $header).invoke('index').then(index => {
+    headerIndex = index
+  })
+  cy.contains('.govuk-table__cell', $defendant).parent().within(() => {
+    if ($data.raw()[0].length > 1) {
+      cy.get('.govuk-table__cell').eq(headerIndex).within(() => {
+        cy.get('.govuk-list').within(() => {
+          $data.raw()[0].forEach((text, index) => {
+            cy.get('li').eq(index).should('contain.text', text)
+          })
+        })
+      })
+    }
+    else {
+      cy.get('.govuk-table__cell').eq(headerIndex).within(() => {
+        cy.get('li').should('not.exist')
+      })
+      cy.get('.govuk-table__cell').eq(headerIndex).should('contain.text', $data.raw()[0])
+    }
+  })
+})
+
 When('I click the date navigation element {int}', $int => {
   cy.get('.pac-days-navigation-item__link').eq($int - 1).click()
 })
