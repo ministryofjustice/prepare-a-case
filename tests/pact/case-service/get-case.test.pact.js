85b14ca269fd06f4efad530d2e000b664b82d4e7
rschallapalli <ravishankar.challapalli@digital.justice.gov.uk>
2022-08-22 12:40:48 +0100

    PIC-2373: fix pact tests

--- tests/pact/case-service/get-case.test.pact.js
+++ tests/pact/case-service/get-case.test.pact.js
@@ -38,7 +38,7 @@ pactWith({ consumer: 'prepare-a-case', provider: 'court-case-service' }, provide
     })
 
     it('should validate the WireMock mocks against the JSON schema', () => {
-      validateMocks(process.env.INIT_CWD + '/mappings/cases', schema, ['create-case-comment.json'])
+      validateMocks(process.env.INIT_CWD + '/mappings/cases', schema, ['create-case-comment.json', 'delete-case-comment.json'])
     })
   })
 })
