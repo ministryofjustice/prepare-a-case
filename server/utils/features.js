676681dcf971f40d2e9beb9d51bec401cfc7c825
rschallapalli <98027245+rschallapalli@users.noreply.github.com>
2022-10-06 17:50:34 +0100

    PIC-2478 enable case tracking features for given courts and users (#674)

--- server/utils/features.js
+++ server/utils/features.js
@@ -1,8 +1,60 @@
-const { enabledForAll, allOf, mainFeatureToggleEnabled, enabledForSourceTypes } = require('./featureTogglePredicates')
+const { allOf, mainFeatureToggleEnabled, enabledForSourceTypes, enabledForCourts, enabledForUsers, isEnv, anyOf } = require('./featureTogglePredicates')
 
 const features = {
-  caseComments: allOf(mainFeatureToggleEnabled('enableCaseComments'), enabledForAll()),
-  caseProgress: allOf(mainFeatureToggleEnabled('enableCaseProgress'), enabledForAll()),
+  caseComments: anyOf(
+    allOf(
+      isEnv('dev'),
+      mainFeatureToggleEnabled('enableCaseComments')
+    ),
+    allOf(
+      isEnv('preprod'),
+      mainFeatureToggleEnabled('enableCaseComments'),
+      enabledForCourts('B50KH'),
+      enabledForUsers('Jo', 'Marg', 'Rebecca', 'Tony')
+    ),
+    allOf(
+      isEnv('prod'),
+      mainFeatureToggleEnabled('enableCaseComments'),
+      enabledForCourts('B50KH'),
+      enabledForUsers('***REMOVED***', 'BEVERLYWILMOTTNPS', 'Jo', 'Marg', 'Rebecca', 'Tony')
+    )
+  ),
+  caseProgress: anyOf(
+    allOf(
+      isEnv('dev'),
+      mainFeatureToggleEnabled('enableCaseProgress')
+    ),
+    allOf(
+      isEnv('preprod'),
+      mainFeatureToggleEnabled('enableCaseProgress'),
+      enabledForCourts('B50KH'),
+      enabledForUsers('Jo', 'Marg', 'Rebecca', 'Tony')
+    ),
+    allOf(
+      isEnv('prod'),
+      mainFeatureToggleEnabled('enableCaseProgress'),
+      enabledForCourts('B50KH'),
+      enabledForUsers('***REMOVED***', 'BEVERLYWILMOTTNPS', 'Jo', 'Marg', 'Rebecca', 'Tony')
+    )
+  ),
+  pastCasesNavigation: anyOf(
+    allOf(
+      isEnv('dev'),
+      mainFeatureToggleEnabled('enablePastCasesNavigation')
+    ),
+    allOf(
+      isEnv('preprod'),
+      mainFeatureToggleEnabled('enablePastCasesNavigation'),
+      enabledForCourts('B50KH'),
+      enabledForUsers('Jo', 'Marg', 'Rebecca', 'Tony')
+    ),
+    allOf(
+      isEnv('prod'),
+      mainFeatureToggleEnabled('enablePastCasesNavigation'),
+      enabledForCourts('B50KH'),
+      enabledForUsers('***REMOVED***', 'BEVERLYWILMOTTNPS', 'Jo', 'Marg', 'Rebecca', 'Tony')
+    )
+  ),
   caseProgressNextAppearanceBadge: enabledForSourceTypes('COMMON_PLATFORM', 'LIBRA')
 }
 
