62edffa69937a06e02ab0fcb0954d5dd02687d6d
Sarahyjja <sarahyjja@github.com>
2022-11-15 12:29:00 +0000

    PIC-2560: Refactoring modal popup

--- public/javascripts/case-progress.js
+++ public/javascripts/case-progress.js
@@ -5,7 +5,6 @@
 
     const selectedForm = document.querySelectorAll('.comments-form-tag textarea')
     selectedForm.forEach(value => {
-      console.log("this is a text area")
       if (multipleNoteBoxFilled) {
         return
       }
@@ -19,25 +18,17 @@
   const popupWrapper = document.getElementById('popup-wrapper')
 
   const goBackBtn = document.getElementById('close-btn')
-  console.log('goBackBtn', goBackBtn)
   goBackBtn.addEventListener('click', () => {
-    console.log("hidding the pop up again...")
     popupWrapper.style.display = "none"
   })
 
   const hearingForms = document.querySelectorAll('.comments-form-tag')
   hearingForms.forEach(form => {
-    console.log('form', form)
     form.addEventListener('submit', (event) => {
-      console.log("we're in the submit")
       if (hasMultipleNoteBoxes(form)) {
-        console.log("cancelling submission (multiple boxes with text found)")
-
         popupWrapper.style.display = "block"
         event.preventDefault()
-        // return false;
       }
-      // return true;
     })
   })
 })()
