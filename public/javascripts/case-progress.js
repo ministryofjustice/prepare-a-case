2aeb9d5d0d3aef13c04c664c4ea989f7530da53f
rschallapalli <ravishankar.challapalli@digital.justice.gov.uk>
2023-03-13 09:54:26 +0000

    PIC-2763 tidy edit note draft

--- public/javascripts/case-progress.js
+++ public/javascripts/case-progress.js
@@ -1,7 +1,7 @@
 (function setupCaseProgressScripts () {
 
   // The timer before saving the draft note after the user paused typing
-  const debounceTimer = 2000;
+  const debounceTimer = 1000;
 
   function getAutoSaveHandler(textarea) {
     const hearingId = textarea.dataset.hearingid
@@ -14,7 +14,7 @@
       timeoutId = setTimeout(function () {
         // Make ajax call to save data.
         const xhr = new XMLHttpRequest()
-        xhr.open('POST', 'summary/auto-save', true)
+        xhr.open('POST', 'summary/auto-save-new-note', true)
         xhr.setRequestHeader('Content-Type', 'application/json')
         xhr.setRequestHeader('x-csrf-token', window.csrfToken)
         xhr.onload = function () {
@@ -40,4 +40,78 @@
   }
 
   document.querySelectorAll('.auto-save-text').forEach(setupAutoSave)
+
+
+  // Edit note
+
+  function getHearingNoteEditsAutoSaveHandler(textarea, noteEditDoneLink) {
+    const hearingId = textarea.dataset.hearingid
+    const noteId = textarea.dataset.noteid
+    let timeoutId;
+
+    const noteEventListener = (event) => {
+      // If a timer was already started, clear it.
+      if (timeoutId) clearTimeout(timeoutId);
+      // Set timer that will save comment when it fires.
+      timeoutId = setTimeout(function () {
+        // Make ajax call to save data.
+        const xhr = new XMLHttpRequest()
+        xhr.open('POST', 'summary/auto-save-note-edits', true)
+        xhr.setRequestHeader('Content-Type', 'application/json')
+        xhr.setRequestHeader('x-csrf-token', window.csrfToken)
+        xhr.onload = function () {
+          if (this.status < 200 || this.status >= 400) {
+            console.log("Error status", this.status)
+          }
+          noteEditDoneLink.classList.remove('govuk-button--disabled')
+          noteEditDoneLink.removeAttribute('disabled')
+        }
+        noteEditDoneLink.setAttribute('disabled', true)
+        noteEditDoneLink.classList.add('govuk-button--disabled')
+        xhr.send(
+          JSON.stringify(
+            {
+              note: event.srcElement.value,
+              hearingId,
+              noteId
+            },
+          )
+        )
+      }, debounceTimer);
+    }
+    return noteEventListener
+  }
+
+  const getNotEditHandler = (hearingNoteDisplayContainer, noteEditContainer, noteReadonlyText, noteEditText, editing, noteEditDoneLink) => {
+
+    const handler = (event) => {
+      event.preventDefault()
+      if (editing) {
+        noteEditText.value = noteReadonlyText.innerText
+        hearingNoteDisplayContainer.setAttribute('hidden', true)
+        noteEditText.addEventListener('keypress', getHearingNoteEditsAutoSaveHandler(noteEditText, noteEditDoneLink))
+        noteEditContainer.removeAttribute('hidden')
+      } else {
+        noteReadonlyText.innerText = noteEditText.value
+        hearingNoteDisplayContainer.removeAttribute('hidden')
+        noteEditContainer.setAttribute('hidden', true)
+      }
+    }
+    return handler
+  }
+
+  const noteContainers = document.querySelectorAll('.hearing-note-container');
+
+  noteContainers.forEach(noteContainer => {
+    const hearingNoteDisplayContainer = noteContainer.querySelector('.hearing-note-display')
+    const noteReadonlyText = hearingNoteDisplayContainer.querySelector('.hearing-note-display-text')
+    const noteEditLink = hearingNoteDisplayContainer.querySelector('.note-edit-link')
+
+    const noteEditContainer = noteContainer.querySelector('.note-edit-container')
+    const noteEditTextArea = noteEditContainer.querySelector('.case-notes-edit')
+    const noteEditDoneLink = noteEditContainer.querySelector('.hearing-note-edit-done')
+    noteEditLink.addEventListener('click', getNotEditHandler(hearingNoteDisplayContainer, noteEditContainer, noteReadonlyText, noteEditTextArea, true, noteEditDoneLink))
+    noteEditDoneLink.addEventListener('click', getNotEditHandler(hearingNoteDisplayContainer, noteEditContainer, noteReadonlyText, noteEditTextArea, false))
+  })
+
 })()
