a258687b8f6437720e31722f9b1959a98426669e
Johny Farrar <me@johnyfarrar.co.uk>
2023-03-22 10:39:33 +0000

    Pic 2828 (#736)
    * :test_tube: Failing tests
    
    * :sparkles: Add AppInsights to FE
    
    * ✅ Fix tests

--- app.js
+++ app.js
@@ -61,6 +61,7 @@ module.exports = function createApp ({ signInService, userService }) {
           'www.google-analytics.com',
           'https://www.google-analytics.com',
           'https://www.googletagmanager.com',
+          'js.monitor.azure.com',
           '\'sha256-6cE0E4X9g7PbRlMR/+GoyLM4W7mjVxZL4H6E8FgY8OA=\'',
           '\'sha256-l1eTVSK8DTnK8+yloud7wZUqFrI0atVo6VlC6PJvYaQ=\'',
           '\'sha256-Ex+PXm59nVbu/S+FH/u8FLio5zO5YfFPo0/jH0uw19k=\'',
@@ -76,7 +77,9 @@ module.exports = function createApp ({ signInService, userService }) {
         connectSrc: [
           '\'self\'',
           'www.google-analytics.com',
-          'https://www.google-analytics.com'
+          'https://www.google-analytics.com',
+          'js.monitor.azure.com',
+          'dc.services.visualstudio.com'
         ]
       }
     }
