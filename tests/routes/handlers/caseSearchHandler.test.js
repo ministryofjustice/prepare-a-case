2212b9224ee5a06dc1c8467ab7874dcfc2b08114
rschallapalli <98027245+rschallapalli@users.noreply.github.com>
2023-04-17 08:58:32 +0100

    PIC-2557 update with common search endpoint (#740)
    * PIC-2557 update with common search endpoint
    
    * PIC-2887 derive and pass search type

--- tests/routes/handlers/caseSearchHandler.test.js
+++ tests/routes/handlers/caseSearchHandler.test.js
@@ -11,20 +11,21 @@ describe('caseSearchHandler', () => {
       },
     render: jest.fn()
   }
-  const crn = 'C123'
-  const mockRequest = { query: { crn } }
+  const term = 'C123456'
+  const mockRequest = { query: { term } }
 
-  const handler = require('../../../server/routes/handlers/getCaseSearchHandler')({ searchByCrn: searchByCrnMock })
+  const getCaseSearchType = require('../../../server/utils/getCaseSearchType')
+  const handler = require('../../../server/routes/handlers/getCaseSearchHandler')({ searchCases: searchByCrnMock }, getCaseSearchType)
 
   it('should invoke the service to search for cases and render template', async () => {
     const data = { items: [] }
     searchByCrnMock.mockResolvedValueOnce({ data })
 
     await handler(mockRequest, mockResponse)
-    expect(searchByCrnMock).toHaveBeenCalledWith(crn)
+    expect(searchByCrnMock).toHaveBeenCalledWith(term, 'CRN')
     expect(mockResponse.render).toHaveBeenCalledWith('case-search', {
       params: mockRequest.params,
-      data: { ...data, crn }
+      data: { ...data, term }
     })
   })
 })
