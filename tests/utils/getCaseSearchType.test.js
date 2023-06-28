9d2675fb7506c66519aadf39a3c0669e0a2eddb6
rschallapalli <98027245+rschallapalli@users.noreply.github.com>
2023-06-28 16:21:57 +0100

    Pic 3099 fix validation (#772)
    * PIC-3099 add validation for non alpha numberic characters
    
    * PIC-3099 add integration test

--- tests/utils/getCaseSearchType.test.js
+++ tests/utils/getCaseSearchType.test.js
@@ -13,6 +13,7 @@ describe('getCaseSearchType', () => {
 
   const CRN_ERROR = 'Enter a CRN in the format one letter followed by 6 numbers, for example A123456.'
   const MISSING_CRN_ERROR = 'You must enter a CRN or a person’s name.'
+  const ERROR_ONLY_ALPHANUMERIC = 'CRNs and names can only contain numbers 0 to 9 and letters A to Z.'
   it.each([
     ['C12345', CRN_ERROR],
     ['C12345D', CRN_ERROR],
@@ -21,8 +22,10 @@ describe('getCaseSearchType', () => {
     [undefined, MISSING_CRN_ERROR],
     ['', MISSING_CRN_ERROR],
     ['a'.repeat(651), 'Name must be less than 650 characters'],
-    ['a'.repeat(650), undefined]
-  ])('given search term %s should return error for invalid input', (input, expecteError) => {
-    expect(getCaseSearchType(input).error).toEqual(expecteError)
+    ['a'.repeat(650), undefined],
+    ['o’hara', ERROR_ONLY_ALPHANUMERIC],
+    ['name & name', ERROR_ONLY_ALPHANUMERIC]
+  ])('given search term %s should return error for invalid input', (input, expectedError) => {
+    expect(getCaseSearchType(input).error).toEqual(expectedError)
   })
 })
