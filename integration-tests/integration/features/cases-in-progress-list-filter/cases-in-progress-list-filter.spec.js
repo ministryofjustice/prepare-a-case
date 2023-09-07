ba2c7bc8940fb7888f195617c2d144df04f4ce41
Pardeep Singh Basi <Pardeep.SinghBasi@digital.justice.gov.uk>
2023-09-07 13:51:46 +0100

    PIC-2883: added in progress tab with tests

---
+++ integration-tests/integration/features/cases-in-progress-list-filter/cases-in-progress-list-filter.spec.js
@@ -0,0 +1,18 @@
+/* global cy */
+import { When } from 'cypress-cucumber-preprocessor/steps'
+
+When('I click the {string} filter button', $string => {
+  cy.get('.pac-filter-button').contains($string).click()
+})
+
+When('I select the {string} filter', $string => {
+  cy.get('[type="checkbox"]').check($string)
+})
+
+When('I click the clear {string} filter tag', $string => {
+  cy.get('.pac-filter__tag').contains($string).click()
+})
+
+When('I click the {string} sort button', $string => {
+  cy.get('.pac-sort-button').contains($string).click()
+})
