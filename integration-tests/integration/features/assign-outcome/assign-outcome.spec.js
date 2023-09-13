796bd3e68aebf3a43a7f016f95637fcab40ee933
Johny Farrar <me@johnyfarrar.co.uk>
2023-09-13 17:36:45 +0100

    🧪 Int tests

---
+++ integration-tests/integration/features/assign-outcome/assign-outcome.spec.js
@@ -0,0 +1,43 @@
+/* global cy expect When And */
+
+And('I should {string} the modal popup to assign hearing outcome', ($expected) => {
+  const expected = $expected.toLowerCase()
+  expect(['see', 'not see']).to.include(expected)
+  cy.get('#assign-outcome-modal').should(`${expected === 'not see' ? 'not.' : ''}be.visible`)
+})
+
+And('the modal popup should have text heading {string}', ($pContent) => {
+  cy.get('#assign-outcome-modal h2').contains($pContent).should('be.visible')
+})
+
+And('the modal popup should have text paragraph {string}', ($pContent) => {
+  cy.get('#assign-outcome-modal p').contains($pContent).should('be.visible')
+})
+
+And('the modal popup should have the button "Assign to me"', () => {
+  cy.get('#assign-outcome-modal button').contains('Assign to me').should('exist')
+})
+
+And('the modal popup should have the link "View without assigning"', () => {
+  cy.get('#assign-outcome-modal a').contains('View without assigning').should('exist')
+})
+
+And('the modal popup should have the close button', () => {
+  cy.get('#assign-outcome-modal button').contains('X').should('exist')
+})
+
+When('I click "Assign to me"', () => {
+  cy.get('#assign-outcome-modal').within(() => {
+    cy.get('.govuk-button').contains('Assign to me').click()
+  })
+})
+
+When('I click "View without assigning"', () => {
+  cy.get('#assign-outcome-modal').within(() => {
+    cy.get('.govuk-link').contains('View without assigning').click()
+  })
+})
+
+When('I click button {string} on hearing outcome modal popup', ($buttonText) => {
+  cy.get('#assign-outcome-modal button').contains($buttonText).click()
+})
