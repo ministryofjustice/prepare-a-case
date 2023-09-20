84e0adb290f9040224409214cfb2307befe144a8
rschallapalli <ravishankar.challapalli@digital.justice.gov.uk>
2023-09-20 09:26:30 +0100

    PIC-2900 add assinged to filter

--- server/utils/getHearingOutcomeAssignedToFilters.js
+++ server/utils/getHearingOutcomeAssignedToFilters.js
@@ -1,15 +1,14 @@
-const flagFilters = require('./flagFilters')
-
-module.exports = (cases, selectedFilters) => {
+module.exports = (cases) => {
   if (!cases || !cases.length) {
     return undefined
   }
 
-  return flagFilters(selectedFilters, [...new Set(cases.map(c => JSON.stringify({ label: c.assignedTo, value: c.assignedToUuid })))]
+  const items = [...new Set(cases.map(c => JSON.stringify({ label: c.assignedTo, value: c.assignedToUuid })))]
     .map(jsonString => JSON.parse(jsonString))
     .sort((a, b) => {
-      const textA = a.label.toUpperCase();
-      const textB = b.label.toUpperCase();
-      return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
-    }))
+      const textA = a.label.toUpperCase()
+      const textB = b.label.toUpperCase()
+      return (textA < textB) ? -1 : (textA > textB) ? 1 : 0
+    })
+  return { id: 'assignedTo', label: 'Assigned to', items }
 }
