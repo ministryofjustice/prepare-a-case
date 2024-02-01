9f3b7011db972d653e25f74d168dbb8447ab46ec
Arron Chave <arron.chave@digital.justice.gov.uk>
2024-02-01 09:20:11 +0000

    PIC-3557: Updated capitalisation

--- server/utils/getOutcomeTypesListFilters.js
+++ server/utils/getOutcomeTypesListFilters.js
@@ -6,7 +6,7 @@ module.exports = () => {
     { label: 'Adjourned', value: 'ADJOURNED' },
     { label: 'Committed to Crown', value: 'COMMITTED_TO_CROWN' },
     { label: 'Crown plus PSR', value: 'CROWN_PLUS_PSR' },
-    { label: 'No Outcome', value: 'NO_OUTCOME' },
+    { label: 'No outcome', value: 'NO_OUTCOME' },
     { label: 'Other', value: 'OTHER' }
   ]
 
