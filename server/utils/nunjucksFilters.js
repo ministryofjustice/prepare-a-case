0e0bbdc4ed20b7190ec4ba2544a4beee6271ef02
ArronChave3 <158048842+ArronChave3@users.noreply.github.com>
2024-02-06 12:08:39 +0000

    improve coverage for nunjucks filters utils (#856)
    * improve coverage for nunjucks filters utils
    
    * remove local command

--- server/utils/nunjucksFilters.js
+++ server/utils/nunjucksFilters.js
@@ -8,20 +8,23 @@ module.exports = {
     return arr ? arr.slice(0, limit) : []
   },
 
-  properCase: (name) => {
+  properCase: name => {
     if (!name) {
       return ''
     }
-    return (name.replace(/[^\s\-']+[\s\-']*/g, (word) => {
-      return word.charAt(0).toUpperCase() + word.substr(1).toLowerCase()
-    }).replace(/\b(Van|De|Der|Da|Von)\b/g, (nobiliaryParticle) => {
-      return nobiliaryParticle.toLowerCase()
-    }).replace(/Mc(.)/g, (match, letter3) => {
-      return `Mc${letter3.toUpperCase()}`
-    }))
+    return name
+      .replace(/[^\s\-']+[\s\-']*/g, word => {
+        return word.charAt(0).toUpperCase() + word.substr(1).toLowerCase()
+      })
+      .replace(/\b(Van|De|Der|Da|Von)\b/g, nobiliaryParticle => {
+        return nobiliaryParticle.toLowerCase()
+      })
+      .replace(/Mc(.)/g, (match, letter3) => {
+        return `Mc${letter3.toUpperCase()}`
+      })
   },
 
-  removeTitle: (name) => {
+  removeTitle: name => {
     const pattern = /\b(?:Mr *|Miss *|Mrs *|Ms *|Dr *|01 )\b/i
     return name ? name.replace(pattern, '') : ''
   },
@@ -36,65 +39,71 @@ module.exports = {
         item = item.replace(',', '')
         hasComma = true
       }
-      return (sourceSplit.includes(item.toLowerCase()) ? `<mark>${item}</mark>` : item) + (hasComma ? ',' : '')
+      return (
+        (sourceSplit.includes(item.toLowerCase())
+          ? `<mark>${item}</mark>`
+          : item) + (hasComma ? ',' : '')
+      )
     })
     return filteredArr.join(' ')
   },
 
-  emptyIfUnmarked: (matchString) => {
+  emptyIfUnmarked: matchString => {
     return matchString.includes('<mark>') ? matchString : ''
   },
 
-  apostropheInName: (name) => {
+  apostropheInName: name => {
     const pattern = /\b(&#39;)\b/g
-    return name.replace(pattern, '\'')
+    return name.replace(pattern, "'")
   },
 
   unique: arr => [...new Set(arr)],
 
-  courtRoomDisplay: (sourceString) => {
-    return sourceString.includes('Courtroom') ? sourceString.replace(/([A-Za-z 0]*)?/, '') : sourceString.replace(/([0]*)?/, '')
+  courtRoomDisplay: sourceString => {
+    return sourceString.includes('Courtroom')
+      ? sourceString.replace(/([A-Za-z 0]*)?/, '')
+      : sourceString.replace(/([0]*)?/, '')
   },
 
-  ordinalNumber: (number) => {
+  ordinalNumber: number => {
     if (!Number.isInteger(number)) return 'Not available'
-    const ordinal = ['st', 'nd', 'rd'][((number + 90) % 100 - 10) % 10 - 1] || 'th'
+    const ordinal =
+      ['st', 'nd', 'rd'][((((number + 90) % 100) - 10) % 10) - 1] || 'th'
     return number + ordinal
   },
 
-  caseCommentTimeFormat: (dateString) => {
+  caseCommentTimeFormat: dateString => {
     if (!dateString) {
       return 'Not available'
     }
     return moment(dateString).format('D MMMM YYYY, HH:mm')
   },
 
-  hearingOutcomeResultedDateFormat: (dateString) => {
+  hearingOutcomeResultedDateFormat: dateString => {
     if (!dateString) {
       return 'Not available'
     }
     return moment(dateString).format('D MMM YYYY \\at HH:mm')
   },
 
-  doubleQuote: (string) => {
+  doubleQuote: string => {
     if (!string) {
       string = ''
     }
     return `&ldquo;${string}&rdquo;`
   },
 
-  caseSearchDateFormat: (dateString) => {
+  caseSearchDateFormat: dateString => {
     if (!dateString) {
       return ''
     }
     return moment(dateString).format('D MMMM YYYY')
   },
 
-  hearingNoteTimeFormat: (dateString) => {
+  hearingNoteTimeFormat: dateString => {
     if (!dateString) {
       return 'Not available'
     }
     return moment(dateString).format('dddd D MMMM YYYY')
   }
-
 }
