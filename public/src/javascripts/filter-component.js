73cd7f1c3628ee7c0297b026d87565abcece6d6e
Arron Chave <arron.chave@digital.justice.gov.uk>
2024-02-19 16:16:28 +0000

    PIC-3267: Simplify filter code, fix for new MOJ library

--- public/src/javascripts/filter-component.js
+++ public/src/javascripts/filter-component.js
@@ -1,5 +1,4 @@
-(function setupFilterComponent () {
-
+;(function setupFilterComponent () {
   'use strict'
 
   function resizeFilterSections () {
@@ -7,58 +6,104 @@
     var filterSections = document.getElementsByClassName('pac-filter-selection')
     Array.prototype.forEach.call(filterSections, function (element, index) {
       element.classList.remove('moj-js-hidden')
-      filterButtons[index].style.width = element.offsetWidth > 1 ? element.offsetWidth + 'px' : filterButtons[index].style.width
+      filterButtons[index].style.width =
+        element.offsetWidth > 1
+          ? element.offsetWidth + 'px'
+          : filterButtons[index].style.width
       element.classList.add('moj-js-hidden')
     })
   }
 
   function configureFilterButtons () {
-    var filterButtons = document.getElementsByClassName('pac-filter-button')
-    var main = document.getElementsByClassName('govuk-main-wrapper')
+    const filterSelectionClass = 'pac-filter-selection'
+    const filterButtonClass = 'pac-filter-button'
+    const filterButtonOpenClass = 'pac-filter-button--open'
+    const filterContainerClass = 'pac-filter-container'
+    const mojHiddenClass = 'moj-js-hidden'
 
-    function checkFiltersFocus (e) {
-      var target = e.target && e.target.id.substr(e.target.id.indexOf('-') + 1)
-      var relatedTarget = e.relatedTarget && e.relatedTarget.id.substr(e.relatedTarget.id.indexOf('-') + 1)
-      if (!target || !relatedTarget || relatedTarget.indexOf(target) === -1) {
-        var filterSections = document.getElementsByClassName('pac-filter-selection')
-        Array.prototype.forEach.call(filterButtons, function (element, index) {
-          element.setAttribute('aria-expanded', false)
-          element.classList.remove('pac-filter-button--open')
-          filterSections[index].classList.add('moj-js-hidden')
-          element.active = false
-        })
-      }
-    }
+    const filterButtons = document.getElementsByClassName(filterButtonClass)
+    const filterSections = document.getElementsByClassName(filterSelectionClass)
+    var main = document.getElementsByClassName('govuk-main-wrapper')
 
     function toggleFilter (e) {
       e.stopPropagation()
-      var $el = e.target
-      var current = !$el.active && $el.dataset['controls']
-      var filterSections = document.getElementsByClassName('pac-filter-selection')
-      Array.prototype.forEach.call(filterButtons, function (element, index) {
-        element.setAttribute('aria-expanded', current === element.dataset['controls'])
-        current === element.dataset['controls'] ? element.classList.add('pac-filter-button--open') : element.classList.remove('pac-filter-button--open')
-        current === filterSections[index].id ? filterSections[index].classList.remove('moj-js-hidden') : filterSections[index].classList.add('moj-js-hidden')
-        if (!$el.active) {
-          element.active = false
-        }
-      })
-      $el.active = !$el.active
+
+      const isFilterButton =
+        e.target.classList !== undefined &&
+        Array.from(e.target.classList).includes(filterButtonClass)
+
+      const isInsideFilterContainer = elementHasParentWithClass(
+        e.target,
+        filterContainerClass
+      )
+
+      if (!e.target.active && isFilterButton) {
+        openFilterMenu(e.target.id)
+      } else if (!isInsideFilterContainer) {
+        closeAllFilters()
+      }
     }
 
-    Array.prototype.forEach.call(filterButtons, function (element) {
-      element.addEventListener('focus', checkFiltersFocus)
-      element.addEventListener('blur', checkFiltersFocus)
-      element.addEventListener('click', toggleFilter)
-    })
+    function elementHasParentWithClass (element, targetClass) {
+      const parents = getAllParents(element)
+      let hasTarget = false
+      parents.forEach(element => {
+        if (Array.from(element.classList).includes(targetClass)) {
+          hasTarget = true
+        }
+      })
+
+      return hasTarget
+    }
+
+    function getAllParents (element) {
+      var a = element
+      var elements = []
+      while (a) {
+        elements.unshift(a)
+        a = a.parentElement
+      }
+
+      return Array.from(elements)
+    }
+
+    function openFilterMenu (filterButtonId) {
+      const filterButtonArray = Array.from(filterButtons)
+      const filterSectionsArray = Array.from(filterSections)
+      const targetButtonIndex = filterButtonArray.findIndex(
+        p => p.id === filterButtonId
+      )
+
+      filterButtons[targetButtonIndex].classList.add(filterButtonOpenClass)
+      filterButtons[targetButtonIndex].active = true
+      filterSections[targetButtonIndex].classList.remove(mojHiddenClass)
+
+      filterButtonArray.splice(targetButtonIndex, 1)
+      filterSectionsArray.splice(targetButtonIndex, 1)
+
+      filterButtonArray.forEach(element => {
+        element.classList.remove(filterButtonOpenClass)
+        element.active = false
+      })
+
+      filterSectionsArray.forEach(element => {
+        element.classList.add(mojHiddenClass)
+      })
+    }
+
+    function closeAllFilters () {
+      Array.from(filterButtons).forEach(element => {
+        element.classList.remove(filterButtonOpenClass)
+        element.active = false
+      })
+
+      Array.from(filterSections).forEach(element => {
+        element.classList.add(mojHiddenClass)
+      })
+    }
 
     Array.prototype.forEach.call(main, function (element) {
       element.addEventListener('click', toggleFilter)
-    })
-
-    var applyFiltersButton = document.getElementsByClassName('pac-apply-filters-button')
-    Array.prototype.forEach.call(applyFiltersButton, function (element) {
-      element.addEventListener('focus', checkFiltersFocus)
     })
   }
 
@@ -70,7 +115,9 @@
       document.getElementById('pac-filter-form').submit()
     }
 
-    var clearTags = document.getElementsByClassName('moj-filter__tag pac-filter__tag')
+    var clearTags = document.getElementsByClassName(
+      'moj-filter__tag pac-filter__tag'
+    )
     Array.prototype.forEach.call(clearTags, function (element) {
       element.addEventListener('click', clearFilter)
     })
@@ -79,13 +126,19 @@
   function configureCheckboxes () {
     function checkboxClick (e) {
       e.stopPropagation()
-      var button = document.getElementById('button-' + e.target.name.substring(e.target.name.indexOf('-') + 1))
+      var button = document.getElementById(
+        'button-' + e.target.name.substring(e.target.name.indexOf('-') + 1)
+      )
       var anySelected = false
-      var checkboxesInGroup = document.querySelectorAll('[id^="checkbox-' + e.target.name + '"]')
+      var checkboxesInGroup = document.querySelectorAll(
+        '[id^="checkbox-' + e.target.name + '"]'
+      )
       Array.prototype.forEach.call(checkboxesInGroup, function (element) {
         anySelected = anySelected || element.checked
       })
-      anySelected ? button.classList.add('pac-filter-button--selected') : button.classList.remove('pac-filter-button--selected')
+      anySelected
+        ? button.classList.add('pac-filter-button--selected')
+        : button.classList.remove('pac-filter-button--selected')
     }
 
     var checkboxes = document.getElementsByClassName('pac-filter-checkbox')
@@ -116,5 +169,4 @@
   configureClearTags()
   configureCheckboxes()
   configureResetButton()
-
 })()
