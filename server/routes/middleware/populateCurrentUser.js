41b11b42acab2c2eeb9fc5e2f2c647fbeea9f137
Johny Farrar <me@johnyfarrar.co.uk>
2024-02-19 13:49:33 +0000

    Update Populate user to bust the cache if items are missing (#865)
    * Update populateCurrentUser.js
    
    * Update cache time

--- server/routes/middleware/populateCurrentUser.js
+++ server/routes/middleware/populateCurrentUser.js
@@ -11,14 +11,18 @@ module.exports = userService => async (req, res, next) => {
         logger.info('Get user credentials from Redis cache.')
       }
       res.locals.user = { ...JSON.parse(getReply), ...res.locals.user }
-      return next()
+      if (!res.locals.user.name || !res.locals.user.userId) {
+        logger.warn('Error getting user credentials from Redis cache.', getReply)
+      } else {
+        return next()
+      }
     }
   }
   try {
     const user = res.locals.user && (await userService.getUser(res.locals.user))
     if (user) {
       res.locals.user = { ...user, ...res.locals.user }
-      await setAsync(`${res.locals.user.username}.INFO`, JSON.stringify(res.locals.user), 'EX', 60 * 60 * 24)
+      await setAsync(`${res.locals.user.username}.INFO`, JSON.stringify(res.locals.user), 'EX', 60 * 60 * 8)
     }
   } catch (error) {
     logger.error(error, `Failed to retrieve user for: ${res.locals.user}`)
