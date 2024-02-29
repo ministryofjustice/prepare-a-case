4a97ae14918964992d7c07b79ff9d36295df6df5
Andrew Charnley <andrew.charnley@digital.justice.gov.uk>
2024-02-29 17:01:43 +0000

    feat: PIC-3622 Workflow Prep Status Assign
    This adds prep status for development env. It can't be merged until the backend has caught up.
    
    In addition a fix for CI not using doker-compose default env values is included.

---
+++ public/src/javascripts/workflow-tasks-prep-status-selector.js
@@ -0,0 +1,43 @@
+'use strict'
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  Array.from(document.getElementsByClassName('workflow-tasks-status-selector'))
+    .forEach(root => {
+
+      let currentSelectedIndex = root.selectedIndex
+
+      // apply 
+      const apply = () => {
+        if (currentSelectedIndex > -1) {
+          root.classList.remove(root.options[currentSelectedIndex].dataset.cssClass)
+        }
+        currentSelectedIndex = root.selectedIndex
+        console.log(currentSelectedIndex, root.options[currentSelectedIndex].dataset.cssClass)
+        root.classList.add(root.options[currentSelectedIndex].dataset.cssClass)
+      }
+
+      // event handler
+      root.addEventListener('change', async () => {
+        try {
+          const body = new URLSearchParams();
+          body.append('state', root.value);
+          const response = await window.fetch(root.dataset['formAction'], {
+            method: 'post',
+            body
+          })
+          if (!response.ok) {
+            throw response
+          }
+          apply()
+        } catch (e) {
+          root.selectedIndex = currentSelectedIndex
+          window.alert(`Error: Unable to change the prep status for ${root.dataset['defendantFullname']}. Try again`)
+        }
+      })
+      
+      // on-load default
+      apply()
+    })
+  
+})
