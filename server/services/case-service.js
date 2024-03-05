e950ee6a1c3af43b2a2256a95e1d149b49a8ab9e
Andrew Charnley <andrew.charnley@digital.justice.gov.uk>
2024-03-05 07:29:51 +0000

    Fix/document upload send auth (#884)
    * fix missing auth header from proxy request
    
    * fix header missing on doc upload getRaw
    
    * fix: missing helm env and template check (where did that go?...)

--- server/services/case-service.js
+++ server/services/case-service.js
@@ -249,7 +249,7 @@ const createCaseService = apiUrl => {
     files: {
       delete: (hearingId, defendantId, fileId) =>
         httpDelete(
-          `${apiUrl}/hearing/${hearingId}/defendant/${defendantId}/files/${fileId}`
+          `${apiUrl}/hearing/${hearingId}/defendant/${defendantId}/file/${fileId}`
         ),
       post: (req, res, next, responseFormatter, hearingId, defendantId) =>
         proxy(apiUrl, {
@@ -258,7 +258,7 @@ const createCaseService = apiUrl => {
             return proxyReqOpts
           },
           proxyReqPathResolver: () => {
-            return `/hearing/${hearingId}/defendant/${defendantId}/files`
+            return `/hearing/${hearingId}/defendant/${defendantId}/file`
           },
           userResDecorator: (proxyRes, proxyResData) => {
             // anything 400+ will be forwarded to next() by the proxy however this still runs so we need to handle
@@ -268,7 +268,7 @@ const createCaseService = apiUrl => {
             if (proxyRes.statusCode === 200) {
               if (!proxyRes.rawHeaders.includes('application/json')) {
                 throw new TypeError(
-                  `Non JSON response for ${apiUrl}/hearing/${hearingId}/defendant/${defendantId}/files`
+                  `Non JSON response for ${apiUrl}/hearing/${hearingId}/defendant/${defendantId}/file`
                 )
               }
               return JSON.stringify(
@@ -276,7 +276,7 @@ const createCaseService = apiUrl => {
               )
             }
             throw new TypeError(
-              `Invalid response status code for ${apiUrl}/hearing/${hearingId}/defendant/${defendantId}/files`
+              `Invalid response status code for ${apiUrl}/hearing/${hearingId}/defendant/${defendantId}/file`
             )
           }
         })(req, res, next),
@@ -295,7 +295,7 @@ const createCaseService = apiUrl => {
             return proxyReqOpts
           },
           proxyReqPathResolver: () => {
-            return `/hearing/${hearingId}/defendant/${defendantId}/files/${fileId}/raw`
+            return `/hearing/${hearingId}/defendant/${defendantId}/file/${fileId}/raw`
           },
           skipToNextHandlerFilter
         })(req, res, next)
