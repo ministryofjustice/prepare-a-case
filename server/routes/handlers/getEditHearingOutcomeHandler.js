8e3e7282234cc6ebf0cf41d14fc208c727f1d782
Johny Farrar <me@johnyfarrar.co.uk>
2024-03-14 13:32:03 +0000

    Typo

--- server/routes/handlers/getEditHearingOutcomeHandler.js
+++ server/routes/handlers/getEditHearingOutcomeHandler.js
@@ -1,6 +1,6 @@
 const getEditHearingOutcomeHandler = ({ addHearingOutcome }) => {
   return async (req, res) => {
-    const { params: { courtCode, hearingId, defendantId }, session, body: { hearingOutcomeType, targetHearingId, addHearingOutcome } } = req
+    const { params: { courtCode, hearingId, defendantId }, session, body: { hearingOutcomeType, targetHearingId, targetdefendantId } } = req
     await addHearingOutcome(targetHearingId, targetdefendantId, hearingOutcomeType)
     session.editHearingOutcomeSuccess = true
     res.redirect(`/${courtCode}/hearing/${hearingId}/defendant/${defendantId}/summary`)
