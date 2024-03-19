9ec9005da1c326c9de89c763e6634765f3e7fb03
Arron Chave <arron.chave@digital.justice.gov.uk>
2024-03-19 14:23:25 +0000

    PIC-3722: Only init the accordion

--- public/config/external.js
+++ public/config/external.js
@@ -1,13 +1,13 @@
 import 'bootstrap/js/dist/modal'
 import $ from 'jquery/src/jquery'
 import { initAll as govuk } from 'govuk-frontend/dist/govuk/all.mjs'
+import { Accordion } from 'govuk-frontend/dist/govuk/all.mjs'
 import { initAll as moj, ButtonMenu } from '@ministryofjustice/frontend/moj/all'
 import '../src/javascripts/caselist-date-carousel'
 
 // govuk() - will break tabs which is why it wasn't previously enabled
 window.$ = $
 moj()
-govuk()
 
 document.body.classList.add('js-enabled')
 
@@ -23,3 +23,28 @@ document.addEventListener('DOMContentLoaded', () => {
       buttonClasses: "govuk-button--secondary moj-button-menu__toggle-button--secondary"
     }))
 })
+
+/* 
+  WARN: Re-implementation of the govuk init all function, this will need reviewing if the package is updated
+  Use to only initialise the accordion, as there are conflicts with the MOJ frontend for other components.
+  This can be expanded to add other specific components, by importing them and adding them to the components array below
+*/
+function initAccordian(config) {
+  let _config$scope;
+  config = typeof config !== 'undefined' ? config : {};
+
+  const components = [[Accordion, config.accordion]];
+  const $scope = (_config$scope = config.scope) != null ? _config$scope : document;
+  components.forEach(([Component, config]) => {
+    const $elements = $scope.querySelectorAll(`[data-module="${Component.moduleName}"]`);
+    $elements.forEach($element => {
+      try {
+        'defaults' in Component ? new Component($element, config) : new Component($element);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+  });
+}
+
+initAccordian()
