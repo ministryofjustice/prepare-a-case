e03ea9e32d7626d151fb7c0a9256eeefe0317fa2
Andrew Charnley <andrew.charnley@digital.justice.gov.uk>
2024-04-09 12:31:19 +0100

    Reintroduce the evil axios-middleware library

--- server/routes/middleware/authorisationMiddleware.js
+++ server/routes/middleware/authorisationMiddleware.js
@@ -1,16 +1,30 @@
 const jwtDecode = require('jwt-decode')
 const config = require('../../config')
 const log = require('../../log')
+const axios = require('axios').default
+
+// TODO WARN: dangerous race conditions - this module is evil
+const { Service } = require('axios-middleware')
+const service = new Service(axios)
 
 module.exports = (req, res, next) => {
+  service.reset()
   // Make sure only users with court admin role can access court app
   if (res.locals?.user?.token) {
     const { authorities: roles, name, user_id: userId, user_uuid: uuid, user_name: username } = jwtDecode(res.locals.user.token)
     Object.assign(res.locals.user, { name, uuid, username, userId })
     if (!roles.includes(config.apis.oauth2.role)) {
-      log.warn(`User does not have required role ${config.apis.oauth2.role}. Ensure Delius user has role 'CWBT200'.`)
+      log.warn(`User does not have required role ${config.apis.oauth2.role}`)
       return res.redirect('/autherror')
     }
+    service.register({
+      onRequest (config) {
+        config.headers = {
+          Authorization: `Bearer ${res.locals.user.token}`
+        }
+        return config
+      }
+    })
     return next()
   }
   // No session: get one created
