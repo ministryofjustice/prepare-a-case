60f201575a7522961ffe2a1988d3f9a8af30486a
Johny Farrar <me@johnyfarrar.co.uk>
2024-04-09 14:44:18 +0100

    Fix: Call Api from case summary post handler (#929)
    * Fix: Call Api from case summary post handler
    
    * Tidy: Unused vars
    
    * Fix: Refactor and improve logic

--- server/routes/handlers/caseSummary.js
+++ server/routes/handlers/caseSummary.js
@@ -1,6 +1,7 @@
 const moment = require('moment')
 const getNextHearing = require('../../utils/getNextHearing')
 const featuresToggles = require('../../utils/features')
+const trackEvent = require('../../utils/analytics')
 
 const {
   settings
@@ -85,7 +86,7 @@ const caseSummaryPostHandler = utils => async (req, res) => {
   const { action } = req.body
   const templateValues = await utils.getCaseAndTemplateValues(req)
 
-  handleButtonAction(templateValues, action, res, req, utils)
+  await handleButtonAction(templateValues, action, res, req, utils)
 }
 
 const isAssignedToUser = (userUuid, hearingOutcome) => {
@@ -99,11 +100,9 @@ const getHearingOutcome = (hearingId, hearings) => {
   return hearing ? hearing.hearingOutcome : null
 }
 
-const handleButtonAction = (templateValues, action, res, req, utils) => {
-  const { caseId, hearingId, defendantId, crn, defendantName } = templateValues.data
+const handleButtonAction = async (templateValues, action, res, req, utils) => {
+  const { caseId, hearingId, defendantId, crn } = templateValues.data
   const { courtCode } = templateValues.params
-  const { apostropheInName, properCase, removeTitle } = utils.nunjucksFilters
-  const formattedName = removeTitle(properCase(apostropheInName(defendantName)))
 
   switch (action) {
     case 'unlinkNdelius':
@@ -111,9 +110,31 @@ const handleButtonAction = (templateValues, action, res, req, utils) => {
     case 'linkNdelius':
       return res.redirect(`/${courtCode}/case/${caseId}/hearing/${hearingId}/match/defendant/${defendantId}/manual`)
     case 'moveToResulted':
-      req.flash('moved-to-resulted', `You have moved ${formattedName}'s case to resulted cases.`)
-      res.redirect(`/${courtCode}/outcomes/in-progress`)
+      await moveToResulted(res, req, utils, templateValues)
   }
+}
+
+const moveToResulted = async (res, req, utils, templateValues) => {
+  const { params: { courtCode, hearingId, defendantId } } = req
+  const { apostropheInName, properCase, removeTitle } = utils.nunjucksFilters
+  const { defendantName } = templateValues.data
+
+  await utils.updateHearingOutcomeToResulted(hearingId, defendantId)
+
+  trackEvent(
+    'PiCPrepareACaseHearingOutcomes',
+    {
+      operation: 'updateHearingOutcomeToResultedFromDefendantSummary',
+      hearingId,
+      courtCode,
+      userId: res.locals.user.userId
+    }
+  )
+
+  const formattedName = removeTitle(properCase(apostropheInName(defendantName)))
+
+  req.flash('moved-to-resulted', `You have moved ${formattedName}'s case to resulted cases.`)
+  res.redirect(`/${courtCode}/outcomes/in-progress`)
 }
 
 const getActionButtons = (templateValues) => {
