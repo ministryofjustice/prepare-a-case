c5f45981dea7dbf4e7678195859f78b030756517
Abdirizak Obsiye <Obsiye@users.noreply.github.com>
2024-04-11 11:43:59 +0100

    Update Pact tests for case comments feature
    Update the Pact tests for case comments by updating the endpoints
    for creating a case comment.
    
    This is because court case service uses the endpoint
    .../cases/:id/defendants/:id/comments
    rather than
    .../cases/:id/comments
    
    This change should fix the PACT tests

--- tests/pact/case-service/delete-case-comment.test.pact.js
+++ tests/pact/case-service/delete-case-comment.test.pact.js
@@ -4,13 +4,16 @@ const { createCaseService } = require('../../../server/services/case-service')
 
 pactWith({ consumer: 'prepare-a-case', provider: 'court-case-service' }, provider => {
   describe('DELETE /cases/{caseId}/comments/{commentId}', () => {
-    it('deletes the case comment with given case id and comment id', async () => {
+    const caseId = 'f76f1dfe-c41e-4242-b5fa-865d7dd2ce57'
+    const defendantId = '062c670d-fdf6-441f-99e1-d2ce0c3a3846'
+
+    it('deletes the case comment with given case id, defendant id and comment id', async () => {
       await provider.addInteraction({
-        state: 'a case exists with the given case id',
+        state: 'a case exists with the given case id and defendant id',
         uponReceiving: 'a request to delete a comment on a case',
         withRequest: {
           method: 'DELETE',
-          path: '/cases/f76f1dfe-c41e-4242-b5fa-865d7dd2ce57/comments/1234'
+          path: `/cases/${caseId}/defendants/${defendantId}/comments/1234`
         },
         willRespondWith: {
           status: 200
@@ -18,7 +21,7 @@ pactWith({ consumer: 'prepare-a-case', provider: 'court-case-service' }, provide
       })
 
       const service = createCaseService(provider.mockService.baseUrl)
-      const response = await service.deleteCaseComment('f76f1dfe-c41e-4242-b5fa-865d7dd2ce57', 1234)
+      const response = await service.deleteCaseComment(caseId,defendantId, 1234)
       expect(response.status).toEqual(200)
     })
   })
