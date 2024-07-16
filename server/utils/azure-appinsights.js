b524d0613cac90eec3981249f2f8608a33dc93f5
Johny Farrar <me@johnyfarrar.co.uk>
2024-07-16 14:14:26 +0100

    Feat: Add user based logging

--- server/utils/azure-appinsights.js
+++ server/utils/azure-appinsights.js
@@ -12,6 +12,23 @@ if (process.env.APPINSIGHTS_INSTRUMENTATIONKEY) {
   module.exports = appInsights.defaultClient
   appInsights.defaultClient.context.tags['ai.cloud.role'] = `${packageData.name}`
   appInsights.defaultClient.context.tags['ai.application.ver'] = buildNumber
+  appInsights.defaultClient.addTelemetryProcessor(addUserDataToRequests)
 } else {
   module.exports = null
+}
+
+function addUserDataToRequests(envelope, contextObjects) {
+  const isRequest = envelope.data.baseType === appInsights.Contracts.TelemetryTypeString.Request
+  if (isRequest) {
+    const { username } = contextObjects?.['http.ServerRequest']?.res?.locals?.user || {}
+    if (username) {
+      const { properties } = envelope.data.baseData
+      // eslint-disable-next-line no-param-reassign
+      envelope.data.baseData.properties = {
+        username,
+        ...properties,
+      }
+    }
+  }
+  return true
 }
