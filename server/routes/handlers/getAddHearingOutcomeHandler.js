71c715840fd35218e2c90c39a8f296241e5c85b4
Johny Farrar <me@johnyfarrar.co.uk>
2024-08-12 13:15:49 +0100

    Feat: Add event for AddHearingOutcome

--- server/routes/handlers/getAddHearingOutcomeHandler.js
+++ server/routes/handlers/getAddHearingOutcomeHandler.js
@@ -1,8 +1,21 @@
+const trackEvent = require('../../utils/analytics')
+
 const getAddHearingOutcomeHandler = ({ addHearingOutcome }) => {
   return async (req, res) => {
     const { params: { courtCode, hearingId, defendantId }, session, body: { hearingOutcomeType, targetHearingId, targetDefendantId } } = req
     await addHearingOutcome(targetHearingId, targetDefendantId, hearingOutcomeType)
     session.addHearingOutcomeSuccess = true
+    trackEvent(
+      'PiCPrepareACaseHearingOutcomes',
+      {
+        operation: 'addHearingOutcome',
+        hearingId,
+        courtCode,
+        defendantId,
+        userId: res.locals.user.uuid,
+        username: res.locals.user.username
+      }
+    )
     res.redirect(`/${courtCode}/hearing/${hearingId}/defendant/${defendantId}/summary`)
   }
 }
