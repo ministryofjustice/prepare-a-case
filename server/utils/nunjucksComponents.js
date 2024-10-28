a1a67355e17e959c56a5103e7f8aa7c3c216653a
ArronChave3 <arron.chave@digital.justice.gov.uk>
2024-10-28 13:14:32 +0000

    Linting

--- server/utils/nunjucksComponents.js
+++ server/utils/nunjucksComponents.js
@@ -3,22 +3,22 @@ const nodePath = require('path')
 const config = require('../../server/config')
 
 const populateTemplateValuesWithComponent = (templateValues, componentName, component) => {
-    if (!templateValues.components) {
-        templateValues.components = {}
-    }
+  if (!templateValues.components) {
+    templateValues.components = {}
+  }
 
-    templateValues.components[componentName] = component
-    return templateValues
+  templateValues.components[componentName] = component
+  return templateValues
 }
 
 const getFilterComponent = (templateValues) => {
-    const templatePath = nodePath.join(__dirname, '../../server/views/components/filter/') + 'template.njk'
-    const filterTemplateValues = { ...templateValues }
-    filterTemplateValues.params.hiddenInputs = templateValues.params.sorts
+  const templatePath = nodePath.join(__dirname, '../../server/views/components/filter/') + 'template.njk'
+  const filterTemplateValues = { ...templateValues }
+  filterTemplateValues.params.hiddenInputs = templateValues.params.sorts
 
-    const componentHtml = nunjucks.render(templatePath, filterTemplateValues)
+  const componentHtml = nunjucks.render(templatePath, filterTemplateValues)
 
-    return { html: componentHtml, javascript: `/javascripts/filter-component-${config.appVersion}.min.js` }
+  return { html: componentHtml, javascript: `/javascripts/filter-component-${config.appVersion}.min.js` }
 }
 
 module.exports = { populateTemplateValuesWithComponent, getFilterComponent }
