816ce31c71d3cba88e7977b18dd2810006558ff0
Daniel Lackenby <11579453+KiraNamida@users.noreply.github.com>
2024-11-19 14:25:28 +0000

    PIC-3156: Create a component for the GDS standard button input and cancel link along with int test steps

---
+++ integration-tests/cypress/support/step_definitions/button-group.js
@@ -0,0 +1,16 @@
+/* global cy expect */
+const { Then } = require('@badeball/cypress-cucumber-preprocessor')
+
+Then('I should have a button group with a button labeled {string}', $label => {
+    cy.get('.govuk-button-group.button-group').should('exist')
+    cy.get('.govuk-button-group.button-group').within(() => {
+        cy.get('.govuk-button.action-button').contains($label).should('exist')
+    })
+})
+  
+Then('I should have a button group with a Cancel link with href {string}', $href => {
+    cy.get('.govuk-button-group.button-group').should('exist')
+    cy.get('.govuk-button-group.button-group').within(() => {
+        cy.get('.govuk-link').contains('Cancel').should('exist').should('have.attr', 'href').and('include', $href)
+    })
+})
