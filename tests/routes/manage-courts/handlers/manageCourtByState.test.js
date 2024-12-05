93726ffa466bb9e9a609e4296a2a6f8754abe238
Daniel Lackenby <daniel.lackenby@digital.justice.gov.uk>
2024-12-05 17:34:47 +0000

    Add unit tests for the extracted manageCourtByState handler's post method

--- tests/routes/manage-courts/handlers/manageCourtByState.test.js
+++ tests/routes/manage-courts/handlers/manageCourtByState.test.js
@@ -1,19 +1,27 @@
 /* global jest, describe, it, expect */
-const { get } = require('../../../../server/routes/manage-courts/handlers/manageCourtByState')
+const { get, post } = require('../../../../server/routes/manage-courts/handlers/manageCourtByState')
 const { settings } = require('../../../../server/config')
 
 describe('manageCourtsByState', () => {
-    describe('GET Handler', () => { 
-        const testBasePath = '/my-courts'
-        const testRequestPath = '/req-path'
+    const testBasePath = '/my-courts'
+    const testNonce = 'test-nonce'
+    const testRequestPath = '/req-path'
+    const testState = 'test-state'
+    const testUsername = 'test-username'
 
+    const defaultCourts = () => ['test-court-A', 'test-court-B']
+
+    const redirectReturnMarker = 'redirect-return-marker'
+    const mockResponse = () => ({
+        render: jest.fn(),
+        redirect: jest.fn(path => redirectReturnMarker),
+        locals: { nonce: testNonce, user: { username: testUsername }}
+    })
+
+    describe('GET Handler', () => { 
         const expectedViewTemplate = 'edit-courts'
-        const expectedUserName = 'test-username'
-        const defaultState = 'default-state'
-        const defaultCourts = () => ['test-court-A', 'test-court-B']
         const validCourt = defaultCourts()[0]
         const invalidCourt = 'test-court-nill'
-        const defaultNonce = 'test-nonce'
 
         const updateSelectedCourtsMock = jest.fn(({}) => ({status: 200}))
         const updateSelectedCourtsMockFailing = jest.fn(({}) => ({status: 400}))
@@ -22,16 +30,11 @@ describe('manageCourtsByState', () => {
             remove: queries.remove ?? null,
             save: queries.save ?? false
         })
-        const mockRequest = (state, queries, courts) => ({
+        const mockGETRequest = (state, queries, courts) => ({
             params: { state },
             query: queries,
             session: { courts },
             path: testRequestPath
-        })
-        const mockResponse = () => ({
-            render: jest.fn(),
-            redirect: jest.fn(path => '/redirect-path'),
-            locals: { nonce: defaultNonce, user: { username: expectedUserName }}
         })
 
         const subject = get(testBasePath, updateSelectedCourtsMock)
@@ -40,17 +43,17 @@ describe('manageCourtsByState', () => {
         const expectedRenderCalledWith = (expected = {}) => ({
             formError: expected.formError ?? false,
             serverError: expected.serverError ?? false,
-            state: expected.state ?? defaultState,
+            state: expected.state ?? testState,
             params: {
                 availableCourts: settings.availableCourts,
                 chosenCourts: expected.courts ?? defaultCourts(),
-                nonce: expected.nonce ?? defaultNonce
+                nonce: expected.nonce ?? testNonce
             }
         })
 
         describe('GIVEN the handler is called with a given state and no specified query parameters', () => {
             it('THEN the template is rendered with the expected default values', async () => {
-                const request = mockRequest(defaultState, requestQueries(), defaultCourts())
+                const request = mockGETRequest(testState, requestQueries(), defaultCourts())
                 const response = mockResponse()
 
                 await subject(request, response)
@@ -61,7 +64,7 @@ describe('manageCourtsByState', () => {
 
         describe('GIVEN that the error query param is flagged', () => {
             it('THEN the template is rendered is the formError template param', () => {
-                const errorRequest = mockRequest(defaultState, requestQueries({error: true}), defaultCourts())
+                const errorRequest = mockGETRequest(testState, requestQueries({error: true}), defaultCourts())
                 const response = mockResponse()
 
                 subject(errorRequest, response)
@@ -74,7 +77,7 @@ describe('manageCourtsByState', () => {
             describe('AND the current session has courts available', () => {
                 describe('WHEN the selected court is found in the available courts', () => {
                     const courtFoundSuite = async () => {
-                        const request = mockRequest(defaultState, requestQueries({remove: validCourt}), defaultCourts())
+                        const request = mockGETRequest(testState, requestQueries({remove: validCourt}), defaultCourts())
                         const response = mockResponse();
                         const result = await subject(request, response)
                         return { request, response, result }
@@ -86,9 +89,10 @@ describe('manageCourtsByState', () => {
                         expect(request.session).toEqual({courts: ['test-court-B']})
                     })
                     it('AND the response is redirected to the request\'s path', async ()=> {
-                        const { request, response } = await courtFoundSuite()
+                        const { request, response, result } = await courtFoundSuite()
 
                         expect(response.redirect).toHaveBeenCalledWith(request.path)
+                        expect(result).toEqual(redirectReturnMarker)
                     })
                     it('AND no render occurs', async () => {
                         const { response } = await courtFoundSuite()
@@ -98,7 +102,7 @@ describe('manageCourtsByState', () => {
                 })
                 describe('WHEN the selected court is not found in the available courts', () => {
                     const courtNotFoundSuite = async () => {
-                        const request = mockRequest(defaultState, requestQueries({remove: invalidCourt}), defaultCourts())
+                        const request = mockGETRequest(testState, requestQueries({remove: invalidCourt}), defaultCourts())
                         const response = mockResponse()
                         const result = await subject(request, response)
                         return {request, response, result}
@@ -117,7 +121,7 @@ describe('manageCourtsByState', () => {
             })
             describe('AND the current session does not have courts available', () => {
                 const noSessionCourtsSuite = async () => {
-                    const request = mockRequest(defaultState, requestQueries({remove: validCourt}), [])
+                    const request = mockGETRequest(testState, requestQueries({remove: validCourt}), [])
                     const response = mockResponse()
                     const result = await subject(request, response)
                     return {request, response, result}
@@ -138,16 +142,16 @@ describe('manageCourtsByState', () => {
         describe('GIVEN that the save query param is flagged', () => {
             describe('AND the current session has courts avaialble', () => {
                 it('THEN updateSelectedCourts is called with tthe expected details', async () => {
-                    const request = mockRequest(defaultState, requestQueries({save: true}), defaultCourts())
+                    const request = mockGETRequest(testState, requestQueries({save: true}), defaultCourts())
                     const response = mockResponse()
 
                     await subject(request, response)
 
-                    expect(updateSelectedCourtsMock).toHaveBeenCalledWith(expectedUserName, defaultCourts())
+                    expect(updateSelectedCourtsMock).toHaveBeenCalledWith(testUsername, defaultCourts())
                 })
                 describe('WHEN the updateSelectedCourts called fails', () => {
                     it('THEN the template is rendered with a serverError', async () => {
-                        const request = mockRequest(defaultState, requestQueries({save: true}), defaultCourts())
+                        const request = mockGETRequest(testState, requestQueries({save: true}), defaultCourts())
                         const response = mockResponse()
 
                         await subjectWithFailingUpdate(request, response)
@@ -157,23 +161,116 @@ describe('manageCourtsByState', () => {
                 })
                 describe('WHEN the updateSelectedCourts call succeeds', () => {
                     it('THEN the response is redirected to the base route', async () => {
-                        const request = mockRequest(defaultState, requestQueries({save: true}), defaultCourts())
+                        const request = mockGETRequest(testState, requestQueries({save: true}), defaultCourts())
                         const response = mockResponse()
 
-                        await subject(request, response)
+                        const result = await subject(request, response)
 
                         expect(response.redirect).toHaveBeenCalledWith(302, testBasePath)
+                        expect(result).toEqual(redirectReturnMarker)
                     })
                 })
             })
             describe('AND the current session does not have courts available', () => {
                 it('THEN the template is rendered with a formError and no courts', async () => {
-                    const request = mockRequest(defaultState, requestQueries({save: true}), [])
+                    const request = mockGETRequest(testState, requestQueries({save: true}), [])
                     const response = mockResponse()
 
                     await subject(request, response)
 
                     expect(response.render).toHaveBeenCalledWith(expectedViewTemplate, expectedRenderCalledWith({formError: true, courts: []}))
+                })
+            })
+        })
+    })
+
+    describe ('POST Handler', () => {
+        const additionalCourt = 'test-court-Z'
+        const existingCourt = defaultCourts()[0]
+
+        const mockPOSTRequest = (state, courts, bodyCourt) => ({
+            params: { state },
+            session: { courts },
+            body: { court: bodyCourt },
+            path: testRequestPath
+        })
+
+        const subject = post(testBasePath)
+
+        describe('GIVEN the handler is called with a given state', () => {
+            describe('AND no court is provided in the request body', () => {
+                const noCourtProvidedSuite = async () => {
+                    const request = mockPOSTRequest(testState, defaultCourts(), undefined)
+                    const response = mockResponse()
+                    const result = await subject(request, response)
+                    return { request, response, result }
+                }
+                it('THEN the response is redirected back to the base route', async () => {
+                    const { response, result } = await noCourtProvidedSuite()
+
+                    expect(response.redirect).toHaveBeenCalledWith(302, expect.stringContaining(testBasePath))
+                    expect(result).toEqual(redirectReturnMarker)
+                })
+                it('AND the given state is maintained', async () => {
+                    const { response, result } = await noCourtProvidedSuite()
+
+                    expect(response.redirect).toHaveBeenCalledWith(302, expect.stringContaining(testState))
+                    expect(result).toEqual(redirectReturnMarker)
+                })
+                it('AND the error query param is provided as true', async () => {
+                    const { response, result } = await noCourtProvidedSuite()
+
+                    expect(response.redirect).toHaveBeenCalledWith(302, expect.stringContaining('?error=true'))
+                    expect(result).toEqual(redirectReturnMarker)
+                })
+            })
+            describe('AND a court is provided in the request body', () => {
+                describe('WHEN the court is not in the existing session\'s courts', () => {
+                    const noCourtsTestCases = [
+                        {name: 'session contains no courts', value: undefined, expectedResult: [additionalCourt]},
+                        {name: 'session contains an empty courts list', value: [], expectedResult: [additionalCourt]},
+                        {name: 'session contains an existing list of courts', value: defaultCourts(), expectedResult: [...defaultCourts(), additionalCourt]},
+                    ]
+                    noCourtsTestCases.forEach(({ name, value, expectedResult }) => {
+                        describe(`- ${name}`, () => {
+                            const courtNotAlreadyInSessionSuite = async () => {
+                                const request = mockPOSTRequest(testState, value, additionalCourt)
+                                const response = mockResponse()
+                                const result = await subject(request, response)
+                                return { request, response, result }
+                            }
+                            it('THEN the request court is added the the session\'s courts', async () => {
+                                const { request } = await courtNotAlreadyInSessionSuite()
+
+                                expect(request.session.courts).toBeDefined()
+                                expect(request.session.courts).toEqual(expectedResult)
+                            })
+                            it('AND the response is redirected to the request\'s path', async () => {
+                                const { request, response } = await courtNotAlreadyInSessionSuite()
+
+                                expect(response.redirect).toHaveBeenCalledWith(request.path)
+                            })
+                        })
+                    })
+                })
+                describe('WHEN the court already exists in the session\'s courts', () => {
+                    const existingCourtSuite = async () => {
+                        const request = mockPOSTRequest(testState, defaultCourts(), existingCourt)
+                        const response = mockResponse()
+                        const result = await subject(request, response)
+                        return { request, response, result }
+                    }
+                    it('THEN the court is not duplicated in the session\'s courts', async () => {
+                        const { request } = await existingCourtSuite()
+
+                        expect(request.session.courts).toBeDefined()
+                        expect(request.session.courts).toEqual(defaultCourts())
+                    })
+                    it('AND the response is redirected to the request\'s path', async () => {
+                        const { request, response } = await existingCourtSuite()
+
+                        expect(response.redirect).toHaveBeenCalledWith(request.path)
+                    })
                 })
             })
         })
