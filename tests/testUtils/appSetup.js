81af2769ffc25cba2ddab37b77e29a8546d29767
ArronChave3 <arron.chave@digital.justice.gov.uk>
2025-05-19 11:32:41 +0100

    Remove catch all logs

--- tests/testUtils/appSetup.js
+++ tests/testUtils/appSetup.js
@@ -29,6 +29,5 @@ module.exports = route => {
   app.use(express.json())
   app.use(express.urlencoded({ extended: false }))
   app.use('/', route)
-  app.use(error => console.log(error))
   return app
 }
