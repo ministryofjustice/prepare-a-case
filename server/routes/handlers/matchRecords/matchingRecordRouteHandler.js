8670a934207f239d58d4a04804d8cf33619cf30c
Lisa Morison <lisa.morison@justice.gov.uk>
2025-09-15 13:31:04 +0100

    fix court code not being passed to match defendant

--- server/routes/handlers/matchRecords/matchingRecordRouteHandler.js
+++ server/routes/handlers/matchRecords/matchingRecordRouteHandler.js
@@ -73,7 +73,6 @@ const matchingRecordRouteHandler = (getMatchDetails, getCaseAndTemplateValues) =
   session.backLink = path
   session.defendantId = defendantId
   session.hearingId = hearingId
-  session.courtCode = courtCode
   res.render('match-defendant', templateValues)
 }
 
