904681b1de8ca14767ef8d8523dffc8969608859
sam.bailey <sam.bailey1@digital.justice.gov.uk>
2025-09-23 09:48:17 +0100

    tidy

--- server/utils/fliptFilter.js
+++ server/utils/fliptFilter.js
@@ -1,20 +1,25 @@
 const { FliptClient } = require('@flipt-io/flipt-client-js');
 const { settings } = require('../config');
 
+const fliptNamespace = 'ProbationInCourt';
+const fliptUpdateInterval = 60000;
+const fliptUrl = settings.flipt.fliptUrl;
+const fliptToken = settings.flipt.fliptToken;  
+
 async function createEvaluator() {
   const fliptClient = await FliptClient.init({
-    url: settings.flipt.fliptUrl,
-    namespace: 'ProbationInCourt',
-    updateInterval: 60000,
+    url: fliptUrl,
+    namespace: fliptNamespace,
+    updateInterval: fliptUpdateInterval,
     authentication: {
-      clientToken: settings.flipt.fliptToken
+      clientToken: fliptToken
     }
   });
 
   return async function evaluate(codeToEval, flagKeyToEval) {
     try {
       const result = await fliptClient.evaluateBoolean({
-        namespaceKey: 'ProbationInCourt',
+        namespaceKey: fliptNamespace,
         flagKey: flagKeyToEval,
         entityId: codeToEval,
         context: {
