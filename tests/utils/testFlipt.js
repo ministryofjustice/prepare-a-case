de5fa2fd9dde5839aaa494c5adb912286ee7cd74
sam.bailey <sam.bailey1@digital.justice.gov.uk>
2025-09-23 09:14:50 +0100

    testing a few courts

--- tests/utils/testFlipt.js
+++ tests/utils/testFlipt.js
@@ -2,9 +2,12 @@ const createEvaluator = require('../../server/utils/fliptFilter.js');
 
 (async () => {
   try {
+    const courtsToTest = [ 'B20BL', 'C20WO', 'B52BB' , 'B50AW', 'B46DB' ];
     const evaluate = await createEvaluator();
-    const result = await evaluate("B44BA", "pic-test");
-    console.log('Feature flag result:', result);
+    for(const element of courtsToTest) {
+      const result = await evaluate(element, "pic-test");
+      console.log('Feature flag result:', result);
+    };
     process.exit(0);
   } catch (err) {
     console.error('Error calling evaluate:', err);
