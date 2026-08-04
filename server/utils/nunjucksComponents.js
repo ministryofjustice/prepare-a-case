1e58a267dde9417562bf0ebddbe11a4a4f568de7
michael.pitt <michael.pitt1@justice.gov.uk>
2026-08-04 15:40:02 +0100

    PCFS-231 - sorting on hearing outcomes

--- server/utils/nunjucksComponents.js
+++ server/utils/nunjucksComponents.js
@@ -14,17 +14,23 @@ const populateTemplateValuesWithComponent = (templateValues, componentName, comp
 const getFilterComponent = (templateValues) => {
   const templatePath = nodePath.join(__dirname, '../../server/views/components/filter/') + 'template.njk'
   const filterTemplateValues = { ...templateValues }
-  const activeSorts = templateValues.params.sorts || []
+  let hiddenInputs
 
-  // Always emit all three sort inputs so filter form submissions preserve whichever sort is active
-  const sortIds = ['hearingDate']
-  if (templateValues.params.includeDefendantSort) sortIds.push('defendantName')
-  if (templateValues.params.includeProbationStatusSort) sortIds.push('probationStatus')
+  if (templateValues.params.sorts) {
+    // Outcomes pages: always emit all three sort inputs so filter submissions preserve whichever sort is active
+    const activeSorts = templateValues.params.sorts
+    const sortIds = ['hearingDate']
+    if (templateValues.params.includeDefendantSort) sortIds.push('defendantName')
+    if (templateValues.params.includeProbationStatusSort) sortIds.push('probationStatus')
 
-  const hiddenInputs = sortIds.map(id => {
-    const active = activeSorts.find(s => s.id === id)
-    return { id, value: active ? active.value : 'NONE' }
-  })
+    hiddenInputs = sortIds.map(id => {
+      const active = activeSorts.find(s => s.id === id)
+      return { id, value: active ? active.value : 'NONE' }
+    })
+  } else {
+    // Non-outcomes pages: don't add sort inputs
+    hiddenInputs = []
+  }
 
   filterTemplateValues.params.hiddenInputs = hiddenInputs
 
